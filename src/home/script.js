fe8dc71024a06300506905a23365b45344e6dec8
damarys <damaryskhawanne10@gmail.com>
2024-06-23 16:32:02 -0300

    criação do header

--- src/home/script.js
+++ src/home/script.js
@@ -146,3 +146,16 @@ async function Signup() {
   }
   console.log("chegou aqui pq ta faltando coisa");
 }
+
+// 
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "/frontend-secti-main/assets/images/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "/frontend-secti-main/assets/images/close_white_36dp.svg";
+  }
+}
+// 
