2e4063f52187f7f82980863bddf4cce2786794bb
Edson Mendes <124837899+EdsonKL@users.noreply.github.com>
2024-06-23 18:11:29 -0300

    Ajustes de pastas e arquivos

--- src/curiosities/script.js
+++ src/curiosities/script.js
@@ -1,12 +1,14 @@
 // 
+const buttonMenuShow = document.getElementById("menuShow").addEventListener("click", menuShow)
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "/frontend-secti-main/assets/images/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "/frontend-secti-main/assets/images/close_white_36dp.svg";
-    }
+  let menuMobile = document.querySelector('.mobile-menu');
+  console.log("chegou aqui")
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "../../assets/images/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "../../assets/images/close_white_36dp.svg";
   }
+}
   // 
--- src/report/script.js
+++ src/report/script.js
@@ -47,14 +47,16 @@ async function fazPost(body) {
 }
 
 // 
+const buttonMenuShow = document.getElementById("menuShow").addEventListener("click", menuShow)
 function menuShow() {
   let menuMobile = document.querySelector('.mobile-menu');
+  console.log("chegou aqui")
   if (menuMobile.classList.contains('open')) {
       menuMobile.classList.remove('open');
-      document.querySelector('.icon').src = "/frontend-secti-main/assets/images/menu_white_36dp.svg";
+      document.querySelector('.icon').src = "../../assets/images/menu_white_36dp.svg";
   } else {
       menuMobile.classList.add('open');
-      document.querySelector('.icon').src = "/frontend-secti-main/assets/images/close_white_36dp.svg";
+      document.querySelector('.icon').src = "../../assets/images/close_white_36dp.svg";
   }
 }
 // 
